dc7d4a6f6ed30a9038eb6ed077ad38df4f24cf10
Challa-pranavchandan <challapranavchandan@gmail.com>
2026-01-21 10:36:50 +0530

    db connect and update README

---
+++ src/index.js
@@ -0,0 +1,48 @@
+//require('dotenv').config({path: './env'}) if disturbs the uniformity of
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: `./env`
+}
+)
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*;( async () => {
+    try{
+       await  mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("err:",error);
+        throw error
+       })
+
+
+       app.listen(process.env.PORT,() => {
+        console.log(`app is running on port ${process.env.PORT}`);
+       })
+
+    }  catch(error){
+        console.error("error",error)
+        throw err
+    }
+}  ) ()*/
+
+
