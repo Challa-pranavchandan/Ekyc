189d0684e5afbab9e32419589f8efead0a764626
Challa-pranavchandan <challapranavchandan@gmail.com>
2026-01-21 12:40:09 +0530

    utils/ayncHandler.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path: './env'}) if disturbs the uniformity of
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -10,7 +11,13 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {app.listen(process.env.PORT,() => {
+        console.log(`app is running on port ${process.env.PORT}`);
+       })})
+.catch((error) => {
+    console.error("Failed to connect to DB", error);
+    process.exit(1);
+})
 
 
 
